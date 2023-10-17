3f7c67a508d9e72c0f56373d8688536b246da65a
Cody Hardy <chardy0919@gmail.com>
2023-10-17 18:01:57 -0400

    Found som bugs

--- Assignments/FlattenList/flatten_list.py
+++ Assignments/FlattenList/flatten_list.py
@@ -8,13 +8,25 @@ def flatten_list(lst):
             new_list.append(item)
     return new_list
 
-def recurse(input, index = 0, new_list = []):
-    if index == len(input):
-        return new_list
-    else:
-        if type(input[index]) == list:
-            for item in input[index]:
-                new_list.append(item)
+# def recurse(input, index = 0, new_list = []):
+#     if index == len(input):
+#         return new_list
+#     else:
+#         if type(input[index]) == list:
+#             for item in input[index]:
+#                 new_list.append(item)
+#         else:
+#             new_list.append(input[index])
+#     return recurse(input, index+1)
+
+def recurse(input, new_list= [],x = 0):
+    # ([1, [2], [3, [4, 5, [6]]], 7])
+    print(new_list)
+    # if x == len(input):
+    #     return new_list
+    for item in input:
+        if type(item) == list:
+            recurse(item)
         else:
-            new_list.append(input[index])
-    return recurse(input, index+1)
+            new_list.append(item)
+    return new_list
