147e73acbbf9619d7d1ccbbee5d05f38b25bc803
Cody Hardy <chardy0919@gmail.com>
2023-10-18 18:21:01 -0400

    got to linked list

--- Assignments/data-structures-I/LinkedList.py
+++ Assignments/data-structures-I/LinkedList.py
@@ -4,3 +4,28 @@
 # remove - remove a given node
 # size - return number of nodes
 # is_empty - True if no nodes, False otherwise
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+    
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def insert(self, value):
+        new_node = Node(value)
+        old_node = self.head
+        self.head = new_node
+        self.head.next = old_node
+
+    def search(self, value):
+        pass
+
+lister = LinkedList()
+lister.insert(1)
+lister.insert(2)
+lister.insert(3)
+lister.insert(4)
+# lister.search(2)
+print(lister.head)
--- Assignments/data-structures-I/Queue.py
+++ Assignments/data-structures-I/Queue.py
@@ -4,3 +4,23 @@
 # peek - return last item
 # size - return number of items
 # is_empty - True if no items, False otherwise
+class Fifo:
+    def __init__(self):
+        self.base = []
+
+    def is_empty(self):
+        return len(self.base) == 0
+
+    def enqueue(self, item):#just like push
+        self.base.append(item)
+           
+    def dequeue(self):
+        return self.base.pop()
+
+    def peek(self):
+        if self.base:
+            return self.base[-1]
+        return None
+    
+    def size(self):
+        return len(self.base)
